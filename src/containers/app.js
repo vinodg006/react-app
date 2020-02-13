5194dc598bd0c67cc711f1234c076c2d5739c09d
Vinod Gupta <vinodg@MITR-G0G1HW.local>
2020-02-13 21:54:43 +0530

    neutral works

--- src/containers/app.js
+++ src/containers/app.js
@@ -15,7 +15,7 @@ let fileData = [];
 class App extends React.Component {
   constructor(props) {
     super(props);
-
+    this.handleClick = this.handleClick.bind(this);
     this.state = {
       start_time: 0,
       end_time: 0,
@@ -35,21 +35,21 @@ class App extends React.Component {
       expand_contract: {
         function: "Roll",
         scope: "Whole File",
-        params: "multiplier",
+        params: { multiplier: 1.05 },
         start_time: 0,
         end_time: 0
       },
       shift: {
         function: "All inputs",
         scope: "Whole File",
-        params: "time",
+        params: { time: -3.5687 },
         start_time: 0,
         end_time: 0
       },
       amplitude: {
         function: "Roll",
         scope: "Whole File",
-        params: "multiplier",
+        params: { multiplier: 0.92, floor: 0.005 },
         start_time: 0,
         end_time: 0
       }
@@ -65,6 +65,27 @@ class App extends React.Component {
   onScopeChange(e, name) {
     this.setState({
       [name]: { ...this.state[name], scope: e.target.value }
+    });
+  }
+
+  handleStart(e, name) {
+    this.setState({
+      [name]: { ...this.state[name], start_time: +e.target.value }
+    });
+  }
+
+  handleStop(e, name) {
+    this.setState({
+      [name]: { ...this.state[name], end_time: +e.target.value }
+    });
+  }
+
+  handleParam(e, name, param) {
+    this.setState({
+      [name]: {
+        ...this.state[name],
+        params: { ...this.state[name].params, [param]: +e.target.value }
+      }
     });
   }
 
@@ -110,13 +131,25 @@ class App extends React.Component {
           <div className="option-container">
             <div style={{ display: "table-cell" }}>Start:</div>
             <div style={{ display: "table-cell" }}>
-              <input type="number" className="input-text" />
+              <input
+                type="number"
+                className="input-text"
+                onChange={e => this.handleStart(e, mapNames[name])}
+                value={this.state[mapNames[name]].start_time}
+                disabled={this.state[mapNames[name]].scope === "Whole File"}
+              />
             </div>
           </div>
           <div className="option-container">
             <div style={{ display: "table-cell" }}>Stop:</div>
             <div style={{ display: "table-cell" }}>
-              <input type="number" className="input-text" />
+              <input
+                type="number"
+                className="input-text"
+                onChange={e => this.handleStop(e, mapNames[name])}
+                value={this.state[mapNames[name]].end_time}
+                disabled={this.state[mapNames[name]].scope === "Whole File"}
+              />
             </div>
           </div>
         </div>
@@ -126,14 +159,68 @@ class App extends React.Component {
               <div className="option-container">
                 <div style={{ display: "table-cell" }}>{val}:</div>
                 <div style={{ display: "table-cell" }}>
-                  <input type="number" className="input-text" />
+                  <input
+                    type="number"
+                    className="input-text"
+                    onChange={e => this.handleParam(e, mapNames[name], val)}
+                    value={this.state[mapNames[name]].params[val]}
+                  />
                 </div>
               </div>
             ))}
-          <button>Apply </button>
+          <button id={mapNames[name]} onClick={this.handleClick}>
+            Apply{" "}
+          </button>
         </div>
       </div>
     );
+  }
+
+  handleClick(e) {
+    const {
+      start_time,
+      end_time,
+      mute,
+      neutral,
+      expand_contract,
+      amplitude: {
+        params: { floor }
+      },
+      shift
+    } = this.state;
+    switch (e.target.id) {
+      case mapNames.Mute: {
+        console.log("1");
+        break;
+      }
+      case mapNames.Neutral: {
+        console.log("2");
+        fileData.forEach((val, index) => {
+          if (val.start >= neutral.start_time && val.end <= neutral.end_time) {
+            fileData[index].lastdata = 0.5;
+          }
+        });
+        console.log(fileData);
+        break;
+      }
+      case mapNames["Expand/Contract"]: {
+        console.log("3");
+        break;
+      }
+      case mapNames.Shift: {
+        console.log("4");
+        break;
+      }
+      case mapNames.Amplitude: {
+        console.log("5");
+        fileData.forEach((val, index) => {
+          if (val.start >= neutral.start_time && val.end <= neutral.end_time) {
+            fileData[index].lastdata = 0.5;
+          }
+        });
+        break;
+      }
+    }
   }
 
   handleFile(e) {
@@ -150,13 +237,31 @@ class App extends React.Component {
       allLines.forEach((line, index) => {
         // console.log(line, index);
         if (line.match(/main_gameplay_start/g)) {
+          const start_time = +line.slice(line.indexOf(":") + 1, -1);
           this.setState({
-            start_time: +line.slice(line.indexOf(":") + 1, -1)
+            start_time,
+            mute: { ...this.state.mute, start_time },
+            neutral: { ...this.state.neutral, start_time },
+            expand_contract: {
+              ...this.state.expand_contract,
+              start_time
+            },
+            shift: { ...this.state.shift, start_time },
+            amplitude: { ...this.state.amplitude, start_time }
           });
         }
         if (line.match(/main_gameplay_end/g)) {
+          const end_time = +line.slice(line.indexOf(":") + 1, -1);
           this.setState({
-            end_time: +line.slice(line.indexOf(":") + 1, -1)
+            end_time,
+            mute: { ...this.state.mute, end_time },
+            neutral: { ...this.state.neutral, end_time },
+            expand_contract: {
+              ...this.state.expand_contract,
+              end_time
+            },
+            shift: { ...this.state.shift, end_time },
+            amplitude: { ...this.state.amplitude, end_time }
           });
         }
         if (line.match(/GAME_CONFIG_END/g)) {
@@ -171,7 +276,9 @@ class App extends React.Component {
           const linemtype = linemend.slice(linemend.indexOf("|") + 1);
           const lastdata = +linemtype.slice(0, linemtype.indexOf("|"));
           const extradata = linemtype.slice(linemtype.indexOf("|") + 1);
-          fileData.push({ start, end, type, lastdata, extradata });
+          if (type === "R" || type === "P" || type === "SE" || type === "TE") {
+            fileData.push({ start, end, type, lastdata, extradata });
+          }
         }
       });
       console.log(fileData, "Vinod");
@@ -183,7 +290,7 @@ class App extends React.Component {
   }
 
   render() {
-    // console.log(this.state);
+    console.log(this.state);
     return (
       <div className="app-container">
         <input type="file" id="file" onChange={this.handleFile.bind(this)} />
